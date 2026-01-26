5bd9defe0c619f0244b5aaf550edf4ac2f93ff0c
Barry Haddow <barry.haddow@gmail.com>
2026-01-26 18:42:32 +0200

    fix slurm script for temp dir

--- docs/scripts/6_glob_aligner.sh
+++ docs/scripts/6_glob_aligner.sh
@@ -1,25 +1,27 @@
 #!/bin/bash -l
 #SBATCH --job-name=glob_align_job           # Job name
-#SBATCH --output=logs/glob_align_job_%A_%a.out    # Stdout (%A = job ID, %a = array index)
-#SBATCH --error=logs/glob_align_job_%A_%a.err     # Stderr (%A = job ID, %a = array index)
+#SBATCH --output=logs/glob_align_%j.out    # Stdout 
+#SBATCH --error=logs/glob_align_%j.err     # Stderr 
 #SBATCH --partition=small             # Partition name
 #SBATCH --ntasks=1                     # One task
 #SBATCH --cpus-per-task=8            # Number of cores
-#SBATCH --time=1-00:00:00              # Run time
+#SBATCH --time=3-00:00:00              # Run time
 #SBATCH --account=project_465001864    # Billing project
 
 module --force purge
 module load LUMI
-module load cray-python
 
-
-# python3 -u 6_alignment_combined.py --folder /fs/alvis0/bhaddow/dochplt/bitexting_v3-clean/da-en/ --output /fs/alvis0/bhaddow/dochplt/bitexting_v3-clean/combined --num_cpu 8 --batch_size 2000
 
 clean_folder=/scratch/project_465001864/bitexting_v3/sharded_data/clean/
 
-python -u ../6_alignment_combined.py \
+
+singularity exec \
+  --env TMPDIR=/flash/project_465001864 \
+  -B /scratch/project_465001864,/flash/project_465001864 \
+  python.sif \
+  python -u 6_alignment_combined.py \
     --folder ${clean_folder}/"$1" \
-    --output ${clean_folder}/combined" \
+    --output ${clean_folder}/combined \
     --num_cpu 8 \
     --batch_size 10000
 
