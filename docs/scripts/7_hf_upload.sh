8e7c9effc04d45af53c9b68aeffe3d198343469b
Barry Haddow <barry.haddow@gmail.com>
2026-02-08 17:13:37 +0200

    script for lumi

--- docs/scripts/7_hf_upload.sh
+++ docs/scripts/7_hf_upload.sh
@@ -4,22 +4,30 @@
 # Options for sbatch
 # ====================
 #SBATCH --job-name="upload_HF"
+#SBATCH --output=logs/upload_%j.out    # Stdout 
+#SBATCH --error=logs/upload_%j.err     # Stderr 
 #SBATCH --account=project_465001864
 #SBATCH --partition=small
 #SBATCH --cpus-per-task=8
 #SBATCH --ntasks=1
 #SBATCH --mem=150G
 #SBATCH --time=24:00:00
-#SBATCH --output=/scratch/project_465001864/malikbha/hplt-doc/logs/output_%j.log
-#SBATCH --error=/scratch/project_465001864/malikbha/hplt-doc/logs/error_%j.log
 
 module --force purge
-module load cray-python
-source /scratch/project_465001864/malikbha/bitextor-slurm/dochplt/bin/activate
+module load LUMI
+clean_folder=/scratch/project_465001864/bitexting_v3/sharded_data/clean/
 
-export HF_DATASETS_CACHE="/scratch/project_465001864/malikbha/hplt-doc/cache"
-export HF_HOME="/scratch/project_465001864/malikbha/hplt-doc/cache"
+export HF_DATASETS_CACHE="/scratch/project_465001864/hf-cache"
+export HF_HOME="/scratch/project_465001864/hf-home"
 #TODO make sure HF_TOKEN is set
 
 
-python /scratch/project_465001864/malikbha/hplt-doc/doc_extraction.py --config="${1}" 
+singularity exec \
+  --env HF_TOKEN=$HF_TOKEN \
+  -B /scratch/project_465001864 \
+  python.sif \
+  python -u 7_hf_upload.py \
+    --config="${1}" 
+
+
+#python /scratch/project_465001864/malikbha/hplt-doc/doc_extraction.py --config="${1}" 
